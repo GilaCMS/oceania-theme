9c476bc3a9f498307e4229a75c2c8d0c02057cd1
Vasilis Zoumpourlis <v.zuburlis@gmail.com>
2019-03-16 06:05:51 -0600

    fix search

--- widgets/search.php
+++ widgets/search.php
@@ -1,4 +1,4 @@
-<form method="get" class="fullwidth" action="<?=gila::make_url('blog')?>" style="position:relative;margin:10px 0">
+<form method="get" class="fullwidth" action="<?=gila::base_url('blog')?>" style="position:relative;margin:10px 0">
 	<input name='search' class="g-input fullwidth" value="<?=(isset($_GET['search'])?$_GET['search']:'')?>" onkeypress="if(event.keyCode==13) submit()">
 	<svg height="24" width="24" style="position:absolute;right:8px;top:8px" viewBox="0 0 28 28">
 		<circle cx="12" cy="12" r="8" stroke="#929292" stroke-width="3" fill="none" />
